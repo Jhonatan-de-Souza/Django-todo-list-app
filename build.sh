c18f98049e36c72a42d72e71f3cde2b1844ca260
jhonatan <jdsjhonatan@hotmail.com>
2025-01-16 08:43:09 -0300

    updating build.sh to run commands in correct directory

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# cd into correct diretory
+cd todo_app
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
