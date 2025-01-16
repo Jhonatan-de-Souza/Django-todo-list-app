98d429a95e0788a004aa57f3985a6a774e370d11
jhonatan <jdsjhonatan@hotmail.com>
2025-01-16 13:20:02 -0300

    modifying the id field in order to make auto generate the id

--- todo_app/todo_list/models.py
+++ todo_app/todo_list/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Todo(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     title = models.TextField(max_length=50, blank=False, null=False)
     status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
