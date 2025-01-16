a21e9fd82c553e900ca48e3032414342556b9f3e
jhonatan <jdsjhonatan@hotmail.com>
2025-01-16 11:12:52 -0300

    changing django secret key and moving it to an env var

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!ksmo(g2+$j63^2wtuh^pf8!ps32d=fy-&7nv1x8fsm0wvgz^*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
